4a719855aedc710b1689b4e552d4fe815434e62c
handy-o <dyhan9420@gmail.com>
2026-09-18 16:00:08 +0900

    [canvas] 장애물 벽 생성/추가

--- three.js/minigame/js/App.js
+++ three.js/minigame/js/App.js
@@ -51,14 +51,44 @@ export default class App {
             //App.ctx.fillRect(50, 50, 100, 100)
 
 
+            // 배경 ----------------------------------
             this.backgrounds.forEach(background => {
                 background.update()
                 background.draw()
             })
-            this.walls.forEach(wall => {
-                wall.update()
-                wall.draw()
-            })
+
+
+            // 벽 ----------------------------------
+            // forEach 시 삭제하면 다음 인스턴스들의 순서에 영향이 생길 수 있어
+            // for문을 사용하여 역순으로 돌 수 있게 수정
+            // this.walls.forEach((wall, i) => {
+            //     wall.update()
+            //     wall.draw()
+                
+            //     // console.log(wall.isOutside)
+            //     if(wall.isOutside) this.walls.splice(i, 1)
+            // })
+
+            for(let i = this.walls.length - 1; i >= 0; i--) {
+                this.walls[i].update()
+                this.walls[i].draw()
+                
+
+                // 벽 제거
+                // console.log(this.walls[i].isOutside)
+                if(this.walls[i].isOutside) {
+                    this.walls.splice(i, 1)
+                    // 제거가 된 인덱스에서는 아래 생성코드가 실행되지 않도록 continue
+                    continue
+                }
+                
+                // 벽 생성
+                if(this.walls[i].canGenerateNext) {
+                    this.walls[i].generatedNext = true
+                    this.walls.push(new Wall({type: Math.random() > 0.3 ? 'SMALL' : 'BIG'}))
+                }
+            }
+            //console.log(this.walls.length)
 
             then = now - (delta % App.interval)
         }
--- three.js/minigame/js/Wall.js
+++ three.js/minigame/js/Wall.js
@@ -1,4 +1,11 @@
 import App from "./App.js"
+import { randomNumBetween } from "./utils.js"
+
+/* 추가 할일 
+1. 벽을 위 아래 위치하여 사이에 간격 랜덤으로 주기 gapY
+2. 오른쪽에서 생성 시켜서 왼쪽으로 이동시키기
+3. 완전히 맵 밖으로 나간 벽은 배열에서 제거 
+*/
 
 export default class Wall {
     constructor(config) {
@@ -16,16 +23,40 @@ export default class Wall {
         }
         this.width = App.height * this.sizeX // 정사각형 이미지 파일 기준으로 this.sizeX의 비율만큼만 보여주기
         this.height = App.height
+
+        this.gapY = randomNumBetween(App.height * 0.2, App.height * 0.35) // 숫자가 낮을수록 어려워짐
+        this.x = App.width // 오른쪽 끝
+        // 최소 -this.height
+        // 최대 App.height - this.gapY - this.height
+        this.y1 = -this.height + randomNumBetween(30, App.height - this.gapY -30) // 풀 조금씩 보이게 30씩 여백 
+        this.y2 = this.y1 + this.height + this.gapY
+        
+        this.generatedNext = false
+        this.gapNextX = App.width * randomNumBetween(0.6, 0.75)
+    }
+    get isOutside() {
+        return this.x + this.width < 0
+    }
+    get canGenerateNext() {
+        return (
+            !this.generatedNext &&
+            this.x + this.width < this.gapNextX
+        )
     }
     update() {
-
+        this.x += -10
     }
     draw(){
         App.ctx.drawImage(
             this.img,
             // 잘라서 보여줄 시작점+크기 sx, sy, sw, sh
             this.sx, 0, this.img.width * this.sizeX, this.img.height,
-            0,0 , this.width, this.height
+            this.x, this.y1 , this.width, this.height
+        )
+        App.ctx.drawImage(
+            this.img,
+            this.sx, 0, this.img.width * this.sizeX, this.img.height,
+            this.x, this.y2 , this.width, this.height
         )
     }
     
